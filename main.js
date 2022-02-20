7206fed89adcc2fc8bd0217dc80cdccda505f3d5
Andre-gomes-dev <gomesandre749@gmail.com>
2022-02-20 15:22:45 -0300

    conhecendo condições e operadores lógicos, permitindo ter um código 'clean'.

--- main.js
+++ main.js
@@ -18,8 +18,11 @@ for(let contador = 0;contador < listaDeTeclas.length;contador++){
         tocaSom(idaudio)
     }
     
-    tecla.onkeydown = function(){
-        tecla.classList.add("ativa")
+    tecla.onkeydown = function(evento){
+       
+        if (evento.code === "Space" || evento.code ===  "Enter"){
+            tecla.classList.add("ativa")
+        }
     } 
     
     tecla.onkeyup = function(){
